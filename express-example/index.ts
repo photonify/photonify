ab03d96b8e46d1b7430154e91df7ff485cbbee7b
Arun Sood <arsood@gmail.com>
2024-01-25 00:39:02 -0800

    Migrate from express-fileupload to Multer

--- express-example/index.ts
+++ express-example/index.ts
@@ -1,26 +1,25 @@
 import express from "express";
-import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
 import path from "path";
+import multer from "multer";
 // import photonify from "../dist/index";
 
 const app = express();
 
+const upload = multer();
+
 //Configure dotenv to set environment variables
 dotenv.config();
-
-//Use the plugin express-fileupload. This plugin simply adds a "files" property to req.
-app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(process.cwd(), "index.html"));
 });
 
+//Test using filesystem storage
+app.post("/filesystem_test", upload.single("photo"), (req, res) => {});
+
 //Test using S3 storage
 app.post("/s3_test", (req, res) => {});
-
-//Test using filesystem storage
-app.post("/filesystem_test", (req, res) => {});
 
 const PORT = 9000;
 
