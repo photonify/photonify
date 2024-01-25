e250b94063833a38791c7e974b6bb255b6fcb472
Arun Sood <arsood@gmail.com>
2024-01-25 09:19:21 -0800

    Start work on remove

--- src/remove.ts
+++ src/remove.ts
@@ -1,1 +1,32 @@
-export function remove() {}
+import fs from "fs";
+import { DeleteObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+import { Settings } from "./types";
+
+export async function remove(
+  settings: Partial<Settings>,
+  pathToFile?: string,
+  fileName?: string
+) {
+  if (settings.storage === "s3") {
+    const client = new S3Client(settings.s3Config);
+
+    const command = new DeleteObjectCommand({
+      Bucket: settings.s3Bucket,
+      Key: fileName,
+    });
+
+    try {
+      await client.send(command);
+      console.log(`Photonify S3 Delete: ${fileName}`);
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    if (pathToFile) {
+      fs.unlinkSync(pathToFile);
+    }
+
+    console.log(`Photonify Local Delete: ${pathToFile}`);
+  }
+}
